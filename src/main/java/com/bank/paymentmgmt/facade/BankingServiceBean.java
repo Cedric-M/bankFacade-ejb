b8e0d094d7b564ee47ccad002984e9c591461d0e
Cedric-M <cirdec95@gmail.com>
2019-05-20 16:18:10 +0200

    Add: Création d’un Session Bean Stateless

--- src/main/java/com/bank/paymentmgmt/facade/BankingServiceBean.java
+++ src/main/java/com/bank/paymentmgmt/facade/BankingServiceBean.java
@@ -1,24 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.bank.paymentmgmt.facade;
 
 import javax.ejb.Stateless;
-import javax.ejb.LocalBean;
 
 /**
  *
- * @author martin
+ * @author ced
  */
 @Stateless
-@LocalBean
-public class NewSessionBean {
 
-    public void businessMethod() {
-    }
+public class BankingServiceBean {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-
 }
