ab8f8028019b4b0d265b04dbebdd1f7d69855617
Cedric-M <cirdec95@gmail.com>
2019-05-21 10:56:27 +0200

    Integration of the DAO (Data access object) into BankingServiceBean

--- src/main/java/com/bank/paymentmgmt/facade/BankingServiceBean.java
+++ src/main/java/com/bank/paymentmgmt/facade/BankingServiceBean.java
@@ -5,7 +5,10 @@
  */
 package com.bank.paymentmgmt.facade;
 
+import com.bank.paymentmgmt.domain.Payment;
+import com.bank.paymentmgmt.integration.PaymentDAO;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.jws.WebService;
 
 /**
@@ -19,12 +22,22 @@ import javax.jws.WebService;
     serviceName = "BankingService"  //optional: nom du <wsdl :service>
 )
 public class BankingServiceBean implements BankingServiceEndpointInterface{
+    
+    @Inject
+    private PaymentDAO paymentDAO;
 
     @Override
     public Boolean createPayment(String ccNumber, Double amount) {
         
         if(ccNumber.length()== 10 ){
             System.out.println("Montant payé : "+amount +" €");
+            Payment p = new Payment();
+            p.setCcNumber(ccNumber);
+            p.setAmount(amount);
+            //pour l’instant le retour n’est pas utilisé
+            p = paymentDAO.add(p);
+            //juste pour tester
+            paymentDAO.getAllStoredPayments();
             return true;
         } else {
             return false;
